76289a230e049a69c732c795c6a044c6e4df2160
Niwesh Sah <sahniwesh@gmail.com>
2024-11-30 02:08:18 +0530

    a

--- DP_group_page/final_code_dp.py
+++ DP_group_page/final_code_dp.py
@@ -137,10 +137,10 @@ class GroupSubmissionApp:
 
     def validate_email(self, email):
         """Validate email format"""
-        email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        # email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
         
         """Validate email format and restrict to iitmandi.ac.in domain."""
-        # email_regex = r'^[a-zA-Z0-9._%+-]+@iitmandi\.ac\.in$'
+        email_regex = r'^[a-zA-Z0-9._%+-]+@iitmandi\.ac\.in$'
         
         return re.match(email_regex, email) is not None
 
